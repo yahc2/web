e15ce53fd7430d9c2734541dda46e775468ed2f3
Raimon Grau <raimonster@gmail.com>
2020-10-23 13:15:39 +0100

    fix .dir-locals.el
    * Now .dir-locals.el works :)
    * Explain how to compile the project in org-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,16 +1,15 @@
-
-((org-publish-project-alist .
-                            '(("org"
-                               :base-directory "./"
-                               :publishing-directory "./docs/"
-                               :publishing-function org-html-publish-to-html
-                               :section-numbers nil
-                               :table-of-contents nil
-                               :html-preamble t
-                               :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/styles/readtheorg/css/htmlize.css\"/>
+((org-mode . ((org-publish-project-alist .
+                                         (("org"
+                                           :base-directory "./"
+                                           :publishing-directory "./docs/"
+                                           :publishing-function org-html-publish-to-html
+                                           :section-numbers nil
+                                           :table-of-contents nil
+                                           :html-preamble t
+                                           :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/styles/readtheorg/css/htmlize.css\"/>
 <link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/styles/readtheorg/css/readtheorg.css\"/>
 
 <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js\"></script>
 <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js\"></script>
 <script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/styles/lib/js/jquery.stickytableheaders.min.js\"></script>
-<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/styles/readtheorg/js/readtheorg.js\"></script>"))))
+<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/styles/readtheorg/js/readtheorg.js\"></script>"))))))
