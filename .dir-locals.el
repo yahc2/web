8a1823cc46cc86dede75ea7cfcabb8fd5405b62f
Wojciech Gac <wga@keepit.com>
2026-08-27 11:49:29 +0200

    Updated URLs for styling and JS

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,12 +4,17 @@
                                            :publishing-directory "./docs/"
                                            :publishing-function org-html-publish-to-html
                                            :section-numbers nil
-                                           :table-of-contents nil
+                                           :with-toc t
                                            :html-preamble t
-                                           :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/htmlize.css\"/>
-<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/readtheorg.css\"/>
+                                           :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg/css/htmlize.css\"/>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg/css/readtheorg.css\"/>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg/css/code-copy.css\"/>
+<link rel=\"stylesheet\" type=\"text/css\" href=\"https://fniessen.github.io/org-html-themes/src/readtheorg/css/search.css\"/>
 
 <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js\"></script>
 <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js\"></script>
-<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/lib/js/jquery.stickytableheaders.min.js\"></script>
-<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/readtheorg_theme/js/readtheorg.js\"></script>"))))))
+<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/vendor/stickytableheaders/jquery.stickytableheaders.min.js\"></script>
+<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/readtheorg/js/search.js\"></script>
+<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/readtheorg/js/readtheorg.js\"></script>
+<script type=\"text/javascript\" src=\"https://fniessen.github.io/org-html-themes/src/readtheorg/js/code-copy.js\"></script>
+"))))))
